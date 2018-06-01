f75033e7cde6bb0be8711af90cc900d205577fd9
Nathan Howell <nath@nhowell.net>
2018-06-01 10:20:37 -0700

    vc settings

--- init.el
+++ init.el
@@ -790,7 +790,8 @@ Close: _c_
   (setq magit-completing-read-function 'ivy-completing-read)
   (add-hook 'git-commit-mode-hook 'evil-insert-state))
 
-(setq auto-revert-check-vc-info t)
+(setq auto-revert-check-vc-info t
+      vc-follow-symlinks t)
 
 
 ;; Installing org-plus-contrib as a lazy workaround for the built-in older org-mode
