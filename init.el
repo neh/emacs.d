6008e0a0d83e5f76f71c91bd3b99231d018214c3
Nathan Howell <nath@nhowell.net>
2019-04-08 17:20:34 -0700

    testing out using org-babel-tangle-file in init

--- init.el
+++ init.el
@@ -1,63 +1,25 @@
 ;; -*- lexical-binding: t -*-
 
-;; This file is pretty much entirely taken from http://www.holgerschurig.de/en/emacs-efficiently-untangling-elisp/
+(setq straight-check-for-modifications '(check-on-save))
 
-;; This is GPLv2. If you still don't know the details, read
-;; http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(defun my-tangle-section-canceled ()
-  "Return t if the current section header was CANCELED, else nil."
-  (save-excursion
-    (if (re-search-backward "^\\*+\\s-+\\(.*?\\)?\\s-*$" nil t)
-        (string-prefix-p "CANCELED" (match-string 1))
-      nil)))
-
-
-;; This uses partially derived code from ob-core.el. So this snippet
-;; is GPLv3 or later. If you still don't know the details, read
-;; http://www.gnu.org/licenses/
-
-(defun my-tangle-config-org (orgfile elfile)
-  "This function will write all source blocks from =config.org= into =config.el= that are ...
-
-    - not marked as :tangle no
-    - have a source-code of =emacs-lisp=
-    - doesn't have the todo-marker CANCELED"
-
-  (let* ((body-list ())
-         (gc-cons-threshold most-positive-fixnum)
-         (org-babel-src-block-regexp (concat
-                                      ;; (1) indentation                 (2) lang
-                                      "^\\([ \t]*\\)#\\+begin_src[ \t]+\\([^ \f\t\n\r\v]+\\)[ \t]*"
-                                      ;; (3) switches
-                                      "\\([^\":\n]*\"[^\"\n*]*\"[^\":\n]*\\|[^\":\n]*\\)"
-                                      ;; (4) header arguments
-                                      "\\([^\n]*\\)\n"
-                                      ;; (5) body
-                                      "\\([^\000]*?\n\\)??[ \t]*#\\+end_src")))
-    (with-temp-buffer
-      (insert-file-contents orgfile)
-      (goto-char (point-min))
-      (while (re-search-forward org-babel-src-block-regexp nil t)
-        (let ((lang (match-string 2))
-              (args (match-string 4))
-              (body (match-string 5))
-              (canc (my-tangle-section-canceled)))
-          (when (and (string= lang "emacs-lisp")
-                     (not (string-match-p ":tangle\\s-+no" args))
-                     (not canc))
-            (add-to-list 'body-list body)))))
-    (with-temp-file elfile
-      (apply 'insert (reverse body-list)))
-    (message "Wrote %s ..." elfile)))
-
-
-;; This is GPLv2. If you still don't know the details, read
-;; http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+(straight-use-package 'org)
 
 (let ((orgfile (concat user-emacs-directory "config.org"))
       (elfile (concat user-emacs-directory "config.el")))
   (when (or (not (file-exists-p elfile))
             (file-newer-than-file-p orgfile elfile))
-    (my-tangle-config-org orgfile elfile))
+    (org-babel-tangle-file orgfile elfile))
   (load-file elfile))
